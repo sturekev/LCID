dcc0a4ffefb4fa8d9d58dfb82e73c232c480a5e7
sturekev <tuphatlqd@gmail.com>
2023-09-28 10:45:21 -0500

    Start feature sendingKey for HallAccess

--- Backend/Authenticate/HallAccess.py
+++ Backend/Authenticate/HallAccess.py
@@ -1,1 +1,8 @@
 #this file contain all of the function suport Hall Access request/verify APis 
+# Feature update UserToken 
+# Feature update UserPublicKey for User Access
+# Feature update UserPrikey for user
+from hash import *
+
+def updateUserToken ():
+    pass

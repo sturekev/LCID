92bc71d1bef52a22bf8de71af16780f16dd48983
flynre01 <flynre01@luther.edu>
2023-10-03 14:49:56 -0500

    slight adjustments

--- Frontend/TestApp/app/home-view-model.js
+++ Frontend/TestApp/app/home-view-model.js
@@ -5,6 +5,18 @@ export function createViewModel() {
     const viewModel = new Observable()
     
   
-   
+    viewModel.logIn = () => {
+  
+      fetch("https://httpbin.org/get")
+      .then((response) => response.json())
+      .then((r) => {
+        viewModel.set("origin", r.url)
+      }).catch((err) => {
+      })
+      viewModel.usr =  viewModel.get('username')
+      viewModel.pass = viewModel.get('password')
+      viewModel.set('debug', `user: ${viewModel.usr} Password: ${viewModel.pass}`)
+    }
+  
     return viewModel
   }

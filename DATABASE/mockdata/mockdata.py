081919e761eb35809be844c4d1011d891d6aae24
Phat Tu <77705393+sturekev@users.noreply.github.com>
2023-10-17 23:15:12 -0500

    Mock Data for DB Guest Account

--- DATABASE/mockdata/mockdata.py
+++ DATABASE/mockdata/mockdata.py
@@ -3,7 +3,10 @@ import bcrypt
 import random
 import string
 import csv
-from hash import *
+from mockdata.hash import *
+
+import psycopg2
+from psycopg2 import OperationalError
 
 
 def generate_random_string(length):
@@ -13,12 +16,15 @@ def generate_random_string(length):
 
 
 
-def create_mockdata():
+def create_mockdata(db_connection):
+    
+    db_cursor = db_connection.cursor()
     # Generate random data and insert it into the guest_account table
     num_of_users = 10  # can change this depends on how many random datapoints needed
     file = open("mockdata.txt", "w")
     file.write("username\t")
-    file.write("password\t")
+    file.write("passwordStr\t")
+    file.write("passwordHashed\t")
     file.write("pubKey\t")
     file.write("user_token\t")
     file.write("token_expiration\n")
@@ -26,21 +32,19 @@ def create_mockdata():
     for _ in range(num_of_users):
         username = generate_random_string(8)  # assuming 8-character usernames
         password = generate_random_string(10)  # assuming 10-character passwords
-        password = hashedPassword(encodePassword(password)) # generate password with 
+        hashedPass = hashedPassword(encodePassword(password)) # generate password with 
         pubKey ,user_token = ExportNewRSAKey()  # 20-character user token
         token_expiration = '2024-12-31'  # sample expiration date, you can randomize this too
 
-        # db_cursor = db_connection.cursor()
-        # db_cursor.execute("INSERT INTO guest_account(username, password, user_token, token_expiration) VALUES (%s, %s, %s, %s);",
-        #                     (username, hashed_password, user_token, token_expiration))
-        # db_connection.commit()
+        db_cursor = db_connection.cursor()
+        db_cursor.execute("INSERT INTO guest_account(username, password, user_token, token_expiration) VALUES (%s, %s, %s, %s);",
+                            (username, hashedPass , user_token, token_expiration))
+        db_connection.commit()
         file.write(username +"\t")
-        file.write(str(password, encoding='utf-8') +"\t")
+        file.write(password +"\t")
+        file.write(str(hashedPass, encoding='utf-8') +"\t")
         file.write(str(pubKey, encoding='utf-8') +"\t")
         file.write(str(user_token, encoding='utf-8') +"\t")
         file.write(token_expiration +"\n")
-    
-
-create_mockdata()
-
+    return db_connection
     

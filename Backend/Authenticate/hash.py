e0b15bd31e5b4978a575e453391ad52353913ac0
sturekev <tuphatlqd@gmail.com>
2023-10-20 01:55:33 -0500

    update

--- Backend/Authenticate/hash.py
+++ Backend/Authenticate/hash.py
@@ -1,42 +1,42 @@
-import bcrypt
-from Crypto.PublicKey import RSA
-from hashlib import sha512
+# import bcrypt
+# from Crypto.PublicKey import RSA
+# from hashlib import sha512
 
-## Password verify
-def encodePassword (password:str): 
-    return password.encode('utf-8')
+# ## Password verify
+# def encodePassword (password:str): 
+#     return password.encode('utf-8')
 
-def hashedPassword (endcodePassword):
-    return bcrypt.hashpw(endcodePassword, bcrypt.gensalt())
+# def hashedPassword (endcodePassword):
+#     return bcrypt.hashpw(endcodePassword, bcrypt.gensalt())
 
-def checkPassword (endcodePassword, hashed: bytes):
-    if bcrypt.checkpw(endcodePassword, hashed):
-        return True
-    return False
-## 
+# def checkPassword (endcodePassword, hashed: bytes):
+#     if bcrypt.checkpw(endcodePassword, hashed):
+#         return True
+#     return False
+# ## 
 
-## RSAGenKey
-def RsaGenKey():
-    keyPair = RSA.generate(bits=1024)
-    # return keyPair.n, keyPair.e, keyPair.d
-    return keyPair 
+# ## RSAGenKey
+# def RsaGenKey():
+#     keyPair = RSA.generate(bits=1024)
+#     # return keyPair.n, keyPair.e, keyPair.d
+#     return keyPair 
 
-# RSA sign the message
-def generateSignature(message: bytes, keyPair_d, keyPair_n):
-    hash = int.from_bytes(sha512(message).digest(), byteorder='big')
-    return pow(hash, keyPair_d, keyPair_n)
-def verifykey (msg,signature,keyPair_e, keyPair_n):
-    hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
-    hashFromSignature = pow(signature, keyPair_e, keyPair_n)
-    return hash == hashFromSignature
+# # RSA sign the message
+# def generateSignature(message: bytes, keyPair_d, keyPair_n):
+#     hash = int.from_bytes(sha512(message).digest(), byteorder='big')
+#     return pow(hash, keyPair_d, keyPair_n)
+# def verifykey (msg,signature,keyPair_e, keyPair_n):
+#     hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
+#     hashFromSignature = pow(signature, keyPair_e, keyPair_n)
+#     return hash == hashFromSignature
 
-def encodeRSAKey ():
-    keyPair = RsaGenKey()
-    PubKey = hex(keyPair.n) + hex(keyPair.e)
-    Prikey = hex(keyPair.n) + hex(keyPair.d)
-    return PubKey, Prikey
+# def encodeRSAKey ():
+#     keyPair = RsaGenKey()
+#     PubKey = hex(keyPair.n) + hex(keyPair.e)
+#     Prikey = hex(keyPair.n) + hex(keyPair.d)
+#     return PubKey, Prikey
 
-def extractRSAkey (key):
-    return key[:258],key[258:]
-##
+# def extractRSAkey (key):
+#     return key[:258],key[258:]
+# ##
 
--- Backend/Authenticate/signin.py
+++ Backend/Authenticate/signin.py
@@ -1,5 +1,5 @@
-from Backend.Assets.Utils import getCurrentDatetime
-from hash import *
+from Assets.Utils import getCurrentDatetime
+from Authenticate.hash import *
 # verify signin username and password from db 
 # return json return for APis (with redirect for 2 step Auth , or a error)
 def signinAuth (username:str, password: str):
@@ -46,5 +46,5 @@ def signinAuth (username:str, password: str) -> dict:
 
 # connect to db and verified it
 # return bool and userToken as str
-def signDb (username: str, password: str) -> bool, str:
+def signDb (username: str, password: str):
     pass

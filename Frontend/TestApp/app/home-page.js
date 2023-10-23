8bcbce15eb7682c0a13f74bb6ec53f8bd4d45330
vuesa01 <97977310+vuesa01@users.noreply.github.com>
2023-10-22 22:02:15 -0500

    added new pages

--- Frontend/TestApp/app/home-page.js
+++ Frontend/TestApp/app/home-page.js
@@ -5,14 +5,26 @@ export function onNavigatingTo(args) {
   page.bindingContext = createViewModel()
 }
 
-export function navigateToMain(args){
+export function onLogout(args) {
   const button = args.object
   const page = button.page
   page.frame.navigate('main-page')
 }
 
-export function onLogout(args) {
+export function onResidenceHall(args) {
   const button = args.object
   const page = button.page
-  page.frame.navigate('main-page')
+  page.frame.navigate('res-hall-page')
+}
+
+export function onDiningService(args) {
+  const button = args.object
+  const page = button.page
+  page.frame.navigate('dining-service-page')
+}
+
+export function onLibraryCheckout(args) {
+  const button = args.object
+  const page = button.page
+  page.frame.navigate('library-checkout-page')
 }
---
+++ Frontend/TestApp/app/library-checkout-view-model.js
@@ -0,0 +1,22 @@
+import { Observable, Fetch } from '@nativescript/core'
+import { debug } from '@nativescript/core/utils'
+
+export function createViewModel() {
+    const viewModel = new Observable()
+    
+  
+    viewModel.logIn = () => {
+  
+      fetch("https://httpbin.org/get")
+      .then((response) => response.json())
+      .then((r) => {
+        viewModel.set("origin", r.url)
+      }).catch((err) => {
+      })
+      viewModel.usr =  viewModel.get('username')
+      viewModel.pass = viewModel.get('password')
+      viewModel.set('debug', `user: ${viewModel.usr} Password: ${viewModel.pass}`)
+    }
+  
+    return viewModel
+  }

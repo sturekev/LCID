6e6d40226606a4206828f146f1988399dd55c276
vuesa01 <97977310+vuesa01@users.noreply.github.com>
2023-11-05 20:39:20 -0600

    dead

--- Frontend/TestApp/app/home-page.js
+++ Frontend/TestApp/app/home-page.js
@@ -1,8 +1,8 @@
-import { createViewModel } from './main-view-model';
+import { createViewModel } from './home-view-model';
 
 export function onNavigatingTo(args) {
   const page = args.object
-  page.bindingContext = createViewModel()
+  // page.bindingContext = createViewModel()
 }
 
 export function onLogout(args) {

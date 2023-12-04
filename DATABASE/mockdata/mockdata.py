557fbd2320c2a82f57f8ceb8398985b1d8393024
Phat Tu <77705393+sturekev@users.noreply.github.com>
2023-12-04 17:30:37 -0600

    database piority

--- DATABASE/mockdata/mockdata.py
+++ DATABASE/mockdata/mockdata.py
@@ -16,35 +16,26 @@ def generate_random_string(length):
 
 
 
-def create_mockdata(db_connection):
+def create_mockdata_account(db_connection=None):
     
     db_cursor = db_connection.cursor()
     # Generate random data and insert it into the guest_account table
-    num_of_users = 10  # can change this depends on how many random datapoints needed
+    num_of_users = 3  # can change this depends on how many random datapoints needed
     file = open("mockdata.txt", "w")
     file.write("username\t")
     file.write("passwordStr\t")
     file.write("passwordHashed\t")
-    file.write("pubKey\t")
-    file.write("user_token\t")
-    file.write("token_expiration\n")
-
     for _ in range(num_of_users):
         username = generate_random_string(8)  # assuming 8-character usernames
         password = generate_random_string(10)  # assuming 10-character passwords
         hashedPass = hashedPassword(encodePassword(password)) # generate password with 
-        pubKey ,user_token = ExportNewRSAKey()  # 20-character user token
-        token_expiration = '2024-12-31'  # sample expiration date, you can randomize this too
 
         db_cursor = db_connection.cursor()
-        db_cursor.execute("INSERT INTO guest_account(username, password, user_token, token_expiration) VALUES (%s, %s, %s, %s);",
-                            (username, hashedPass , user_token, token_expiration))
+        db_cursor.execute("INSERT INTO guest_account(username, password) VALUES (%s, %s,);",
+                            (username, hashedPass))
         db_connection.commit()
         file.write(username +"\t")
         file.write(password +"\t")
         file.write(str(hashedPass, encoding='utf-8') +"\t")
-        file.write(str(pubKey, encoding='utf-8') +"\t")
-        file.write(str(user_token, encoding='utf-8') +"\t")
-        file.write(token_expiration +"\n")
     return db_connection
     

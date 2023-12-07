07f6b68510f57ba25c80ffd341d5908dd22d2dda
flynre01 <flynre01@luther.edu>
2023-12-07 10:55:23 -0600

    demo up to date

--- Frontend/TestApp/app/apiUrls.js
+++ Frontend/TestApp/app/apiUrls.js
@@ -1,5 +1,6 @@
 module.exports = {
     apilogin: "http://10.0.2.2:8000/login/",
     apiHallAccess: "http://10.0.2.2:8000/HallAccess/me/",
-    apiDiningAccess: "http://10.0.2.2:8000/dinningservice/caf/me/1"
+    apiDiningAccess: "http://10.0.2.2:8000/dinningservice/caf/me/1",
+    apiLibrayID: "http://10.0.2.2:8000/library/me/"
 };

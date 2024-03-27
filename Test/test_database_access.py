67f702a86616baa503fa5508dd9e68b6d430b1c2
vuesa01 <97977310+vuesa01@users.noreply.github.com>
2024-03-27 13:06:22 -0500

    initial commit

--- Test/test_database_access.py
+++ Test/test_database_access.py
@@ -1,8 +1,18 @@
+import sys
 import pytest
 
 from fastapi.testclient import TestClient
 
+sys.path.insert(1, '../Backend/')
+sys.path.insert(1, '../Backend/Assets')
+# sys.path.append('../Backend/DinningService')
+from DinningService.caf import verify_caf_swipe
 from db_connection import get_db_connection
+
+from jsonFormat import UserInDB
+from main import app, get_current_active_user
+
+client = TestClient(app)
 
 @pytest.fixture(scope="module")
 def local_db_connection():
@@ -14,7 +24,19 @@ def local_db_connection():
 def aws_db_connection():
     db_connection = get_db_connection()
     yield db_connection
-    db_connection.close()    
+    db_connection.close()
+
+async def override_dependency_right():
+    mock_user_right = {
+       "username": "tuph01",
+        "full_name": "Kevin Tu",
+        "email": "tuph01@luther.edu",
+        "disabled": False,
+        "student_id": 529194,
+        "hashed_password": "$2b$12$pzXAzMq09IhPZjcJ7c.xq.vdJ4dE7307BlJAUBh7G2pzKAd4NfjEm"
+    }
+    user_data = UserInDB(**mock_user_right)
+    return user_data    
 
 def test_local_database_connection_success(local_db_connection):
     cursor = local_db_connection.cursor()
@@ -113,7 +135,10 @@ def test_users_retrieve_all_failure(local_db_connection, aws_db_connection):
     assert local_result != aws_result, "User does not exist in one of the databases"
 
 def test_meal_swipe_updated():
-    ...
+    app.dependency_overrides[get_current_active_user] = override_dependency_right
+    swipes = 2
+    response_caf_me = client.get(f"/dinningservice/caf/me/{swipes}")
+    assert response_caf_me.status_code == 200
 
 def test_meal_swipe_not_updated():
     ...

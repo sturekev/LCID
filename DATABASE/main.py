42240e08b3fedd6631fc0410f5ec5ea8a61fecf0
vuesa01 <97977310+vuesa01@users.noreply.github.com>
2024-03-31 20:12:01 -0500

    file update

--- DATABASE/main.py
+++ DATABASE/main.py
@@ -8,9 +8,9 @@ section='cardReaderDB'
 db_info = get_db_info(filename, section)
 
 db_connection = None
-try:
-    db_connection = psycopg2.connect(**db_info)
-    print("Successfully connected to the database.")
+
+db_connection = psycopg2.connect(**db_info)
+print("Successfully connected to the database.")
 
     # db_cursor = db_connection.cursor()
     # create_table = '''DROP TABLE IF EXISTS account CASCADE;
@@ -63,32 +63,16 @@ try:
     # print("Records inserted successfully to meal balance table")
     # db_connection.commit()
 
-except OperationalError:
-    print("Error connecting to the database :/")
-    print(OperationalError)
+print("\nRetrieval from database imminent...")
 
-finally:
-    if db_connection:
-        db_connection.close()
-        print("Closed connection.")
+db_cursor = db_connection.cursor()
+db_cursor.execute('''SELECT fullname, username, password, id_number, building_name 
+                    FROM account, account_profile, building_info 
+                    WHERE account_profile.account_id = account.id 
+                    AND building_info.building_id = account_profile.housing''')
+info_result = db_cursor.fetchall()
+print(info_result)
 
-try:
-    db_connection = psycopg2.connect(**db_info)
-    print("\nRetrieval from database imminent...")
-    
-    db_cursor = db_connection.cursor()
-    db_cursor.execute('''SELECT fullname, username, password, id_number, building_name 
-                        FROM account, account_profile, building_info 
-                        WHERE account_profile.account_id = account.id 
-                        AND building_info.building_id = account_profile.housing''')
-    info_result = db_cursor.fetchall()
-    print(info_result)
-
-except OperationalError:
-    print("Error retrieving data :/")
-    print(OperationalError)
-
-finally:
-    if db_connection:
-        db_connection.close()
-        print("Closed connection.")
+db_connection.close()
+print("Closed connection.")
+        

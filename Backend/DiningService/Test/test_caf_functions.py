6d0564b28bcb0cfb42ee9e327bd35c1e72884357
vuesa01 <97977310+vuesa01@users.noreply.github.com>
2024-04-02 17:38:50 -0500

    refactored DiningService files

--- Backend/DiningService/Test/test_caf_functions.py
+++ Backend/DiningService/Test/test_caf_functions.py
@@ -5,21 +5,21 @@ sys.path.insert(1, '../Backend')
 from DiningService.caf import get_user_caf_db, get_account_id_db, update_user_swipe_db
 
 def test_get_user_caf_db():
-    studentId = "528480"
-    query_result = get_user_caf_db(studentId)
+    student_id = "528480"
+    query_result = get_user_caf_db(student_id)
 
-    assert studentId in query_result
+    assert student_id in query_result
     assert query_result['528480']['student_id'] == '528480'
     assert query_result['528480']['swipes'] == 100
     assert query_result['528480']['dining_dolars'] == 183.89
     assert query_result['528480']['role'] == 'Student'
 
 def test_get_account_id_db():
-    studentId = "528480"
-    accountId = 1
-    query_result = get_account_id_db(studentId)
+    student_id = "528480"
+    account_id = 1
+    query_result = get_account_id_db(student_id)
 
-    assert accountId == query_result
+    assert account_id == query_result
 
 def test_update_user_swipe_db():
     test_student_pk_number = 3

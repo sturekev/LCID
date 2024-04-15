e621c50e8d6bf1acee5eaf3260495c42d8e1e57d
sturekev <tuphatlqd@gmail.com>
2024-04-14 22:04:06 -0500

    update frame

--- qrcodeDetector/send_data1.py
+++ qrcodeDetector/send_data1.py
@@ -5,7 +5,7 @@ import datetime
 def send_data1(token):
     # Your secret key to sign the token. Keep it secret!
     SECRET_KEY = "60528e0f7a9ae5946ddasdd2d012f3904ccf2c7vbe227zxcv79b300095855b429b61303"
-    base_url = "http://127.0.0.1:8000"  # Replace with the actual URL of your FastAPI server
+    base_url = "http://18.215.231.250"  # Replace with the actual URL of your FastAPI server
 
     response1 = requests.get(f"{base_url}/echo/{token}")
 #  Print the response from the server

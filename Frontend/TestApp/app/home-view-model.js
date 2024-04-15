82d4505c078106e0d8d6e0c026d51404c576663f
flynre01 <98500902+flynre01@users.noreply.github.com>
2024-04-14 22:31:15 -0500

    Bug fixes and refactoring
    Fixed no data loading in home dashboard and fixed typo in apiDiningAccess that caused qr-code on Dining services page to not load. Also removed som excess code that remained from testing phase.

--- Frontend/TestApp/app/home-view-model.js
+++ Frontend/TestApp/app/home-view-model.js
@@ -1,6 +1,6 @@
-import { Observable, Fetch } from '@nativescript/core'
+import { Observable } from '@nativescript/core'
 import {Http } from '@nativescript/core';
-import { getString, setString } from '@nativescript/core/application-settings'
+import { getString } from '@nativescript/core/application-settings'
 var getRequests = require("./apiUrls")
 
 export function createViewModel() {
@@ -14,14 +14,12 @@ export function createViewModel() {
       headers: { 'Content-Type' : 'application/json', 'Authorization' : 'Bearer ' + getString('access_token')},
     }).then(response => {
       var result = response.content.toJSON();
-      var res = result.message
-      console.log(res)
-      if(res != null){ 
-        viewModel.set('name', `Name: ${res.full_name}`)
-        viewModel.set('sid', `Student ID: ${res.student_id}`)
-        viewModel.set('dorm', `Residence: ${res.residence}`)
-        viewModel.set('swipes', `Meal Swipes: ${res.swipes}`)
-        viewModel.set('dinedlrs', `Dining Dollars: ${res.dining_dolar}`)
+      if(result != null){ 
+        viewModel.set('name', `Name: ${result.full_name}`)
+        viewModel.set('sid', `Student ID: ${result.student_id}`)
+        viewModel.set('dorm', `Residence: ${result.residence}`)
+        viewModel.set('swipes', `Meal Swipes: ${result.swipes}`)
+        viewModel.set('dinedlrs', `Dining Dollars: ${result.dining_dolar}`)
       }
       else{
         viewModel.set('debug', ` resToken: ${result}`);
--- Frontend/TestApp/app/library-checkout-view-model.js
+++ Frontend/TestApp/app/library-checkout-view-model.js
@@ -1,22 +1,6 @@
-import { Observable, Fetch } from '@nativescript/core'
-import { debug } from '@nativescript/core/utils'
+import { Observable } from '@nativescript/core'
 
 export function createViewModel() {
     const viewModel = new Observable()
-    
-  
-    viewModel.logIn = () => {
-  
-      fetch("https://httpbin.org/get")
-      .then((response) => response.json())
-      .then((r) => {
-        viewModel.set("origin", r.url)
-      }).catch((err) => {
-      })
-      viewModel.usr =  viewModel.get('username')
-      viewModel.pass = viewModel.get('password')
-      viewModel.set('debug', `user: ${viewModel.usr} Password: ${viewModel.pass}`)
-    }
-  
     return viewModel
   }
